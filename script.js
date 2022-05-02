abb680c3ef262cd5368a0deb7e5eed4599842496
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-02 18:22:01 +0200

    dino

---
+++ script.js
@@ -0,0 +1,5 @@
+const dino1 = new Dinosaur('Giganotosaurus', 'Charcarodontosaurid', '99.6-97 mya', 'South America', 'c', [12, 13.2], [4.2, 13.8, 6.8, 10.4])
+console.log(dino1); 
+console.log('Length: ', dino1.maxSize()); 
+console.log('Weight: ', dino1.maxWeight()); 
+console.log(dino1.toString()); 
